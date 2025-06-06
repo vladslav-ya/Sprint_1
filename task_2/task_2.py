f4662d7018efaf4868c75449994fa58c9622df0b
svyatoslavskij.v <svyatoslavskij.v@ct-sg.ru>
2025-06-06 19:44:17 +0300

    task_5

--- task_2/task_2.py
+++ task_2/task_2.py
@@ -10,6 +10,6 @@ class Tester:
             print(self.name, 'Можно отдыхать')
 
 tester_1 = Tester(name='tester_1')
-tester_1.work_hard(deadline=False)  # 'tester_1 Можно отдыхать'
+tester_1.work_hard(deadline=False)
 tester_2 = Tester(name='tester_2')
-tester_2.work_hard(deadline=True)   # 'tester_2 Что ж, ещё часок поработаю!'
+tester_2.work_hard(deadline=True)
